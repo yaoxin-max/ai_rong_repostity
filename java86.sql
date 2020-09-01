f073ae5d960ed169ae82c5316cd5c9df12a8b229
yaoxin <yx17693121776@163.com>
2020-09-01 10:53:07 +0800

    第一次保存

---
+++ java86.sql
@@ -0,0 +1,52 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : java86
+Source Server Version : 50515
+Source Host           : localhost:3306
+Source Database       : java86
+
+Target Server Type    : MYSQL
+Target Server Version : 50515
+File Encoding         : 65001
+
+Date: 2020-05-29 11:48:21
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `role`
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `roleId` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `roleName` varchar(30) DEFAULT NULL COMMENT '角色名称',
+  PRIMARY KEY (`roleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('1', '普通用户');
+INSERT INTO `role` VALUES ('2', '会员');
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `username` varchar(30) DEFAULT NULL COMMENT '用户名称',
+  `roleId` int(11) DEFAULT NULL COMMENT '外键约束',
+  PRIMARY KEY (`id`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `role` (`roleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', '姚鑫', '1');
+INSERT INTO `user` VALUES ('2', '姚磊', '1');
+INSERT INTO `user` VALUES ('3', '姚垚', '2');
